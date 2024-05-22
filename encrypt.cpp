840f90dca69bb9852e971e54a6c235a2ecd8db74
prarthik krishnan <prarthik2003@gmail.com>
2024-05-21 19:02:40 -0500

    init commit working excrypt decrypt

---
+++ encrypt.cpp
@@ -0,0 +1,109 @@
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using dir_iter = std::filesystem::recursive_directory_iterator;
+
+// char key = 'b';
+
+void encrypt (std::fstream& encryption, char key) {
+    char read;
+    if(encryption.is_open()){
+        while(!encryption.eof()){
+            read = encryption.get();
+            encryption.seekp((encryption.tellp() - static_cast<std::streampos>(1)));
+            encryption.put(read ^ key);
+            encryption.seekp(encryption.tellp());
+        }
+    }
+}
+
+int check (char key){
+    std::fstream encryption ("checksum.txt");
+    std::stringstream buf;
+    buf << encryption.rdbuf();
+    std::string st = buf.str();
+    encryption.close();
+    for(size_t i = 0; i < st.length(); ++i){
+        st[i] = st[i] ^ key;
+    }
+    std::cout << "st: " << st << std::endl;
+    std::string exp = "checkcheckcheckycheck\n\n";
+    for(size_t i = 0; i < st.length(); ++i){
+        if(st[i] != exp[i]) {
+            std::cout << i<< std::endl;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main (){
+    // std::fstream checksum ("checksum.txt");
+    std::string file;
+    char key;
+    char inp;
+    // std::string check_text;
+
+    int flag = 0;
+    int enc_de = 0;
+
+    for(const auto& dirEntry : dir_iter(std::filesystem::current_path())){
+        file = dirEntry.path().generic_string();
+        if(file.find("checksum") != std::string::npos) flag = 1;
+    }
+
+    if(flag == 0){
+        std::ofstream checksum ("checksum.txt");
+        checksum << "checkcheckcheckycheck\n" << std::endl;
+
+        checksum.close();
+    }
+    
+    for(const auto& dirEntry : dir_iter(std::filesystem::current_path())){
+        file = dirEntry.path().generic_string();
+        if((file.find("a.exe") != std::string::npos) | (file.find("encrypt.cpp") != std::string::npos)) continue;
+        std::cout << file << std::endl;
+    }
+
+    std::fstream checksum_i ("checksum.txt");
+    std::cout << "Encrypt or decrypt? E/D" << std::endl;
+    std::cin >> inp;
+
+    std::cout << "Enter key: " << std::endl;
+    std::cin >> key;
+
+    std::cout<< inp << key << std::endl;
+
+
+    // checksum_i.open("checksum.txt");
+
+    if((inp == 'D') && (check(key) == 0)){
+        std::cout << "great!" << std::endl;
+        for(const auto& dirEntry : dir_iter(std::filesystem::current_path())){
+            file = dirEntry.path().generic_string();
+            if((file.find("a.exe") != std::string::npos) | (file.find("encrypt.cpp") != std::string::npos)) continue;
+            std::cout << file << std::endl;
+            std::fstream rd (file);
+            encrypt(rd, key);
+        }
+    }
+    else if ((inp == 'E')){
+        for(const auto& dirEntry : dir_iter(std::filesystem::current_path())){
+            file = dirEntry.path().generic_string();
+            if((file.find("a.exe") != std::string::npos) | (file.find("encrypt.cpp") != std::string::npos)) continue;
+            std::cout << file << std::endl;
+            std::fstream rd (file);
+            encrypt(rd, key);
+        }
+    }
+    else {
+        std::cout << ":( no go" << std::endl;
+
+    }
+    return 0;
+}
+
+
